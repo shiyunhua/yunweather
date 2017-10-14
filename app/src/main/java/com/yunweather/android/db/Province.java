84b86ae8b9f6c4a1a4accb878cb2ac8025aec1ab
wilson <2879455450@qq.com>
2017-10-14 22:14:14 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/yunweather/android/db/Province.java
@@ -0,0 +1,35 @@
+package com.yunweather.android.db;
+
+/**
+ * Created by Wilson on 2017/10/14.
+ */
+
+public class Province extends  DataSupport {
+	private int id;
+	private String provinceName;
+	private int provinceCode;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public int getProvinceCode() {
+		return provinceCode;
+	}
+
+	public void setProvinceCode(int provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+}
